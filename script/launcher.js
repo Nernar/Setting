9a999611c81682da982a5a774ce0aeaea3a4d24d
MaXFeeD <46795182+MaXFeeD@users.noreply.github.com>
2024-01-07 04:17:23 +0300

    Additional options in launcher

--- script/launcher.js
+++ script/launcher.js
@@ -1,25 +1,21 @@
-const launchModification = function(additionalScope) {
-	if (this.isInstant !== undefined) {
-		return;
-	}
-	if (additionalScope !== undefined) {
-		__mod__.RunMod(additionalScope);
-		return;
-	}
-	Launch();
-};
-
-(function() {
+(function(context) {
 	try {
-		ConfigureMultiplayer({
-			isClientOnly: true
-		});
+		Launch(context);
 	} catch (e) {
-		launchModification({
-			isOutdated: true
-		});
-		return;
+		__mod__.RunMod(context);
 	}
-
-	launchModification();
-})();
+})({
+	Level: ModAPI.requireGlobal("Level"),
+	canTileBeReplaced: ModAPI.requireGlobal("canTileBeReplaced"),
+	isOutdated: (function() {
+		try {
+			ConfigureMultiplayer({
+				isClientOnly: true
+			});
+			return false;
+		} catch (e) {
+			Logger.Log("Setting: Environment has been deprecated, it may cause unexpected behavior!", "WARNING");
+		}
+		return true;
+	})()
+});
